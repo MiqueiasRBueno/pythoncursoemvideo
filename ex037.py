d62443be1008c640ae39963889aa84d84fd0c79e
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-03-05 13:36:38 -0300

    add dos conversores!

--- ex037.py
+++ ex037.py
@@ -4,8 +4,10 @@ num = int(input('Digite um número qualquer: '))
 print('Base para conversão\n- 1 para binário\n- 2 para octal\n- 3 para hexadecimal')
 conv = input('Escolha a base de conversão: ')
 if conv == '1':
-    print(f'O número {num} convertido em binário é ')
+    print(f'O número {num} convertido em binário é {bin(num)[2:]}')
 elif conv == '2':
-    print(f'O número {num} convertido em octal é ')
+    print(f'O número {num} convertido em octal é {oct(num)[2:]}')
 elif conv == '3':
-    print(f'O número {num} convertido em hexadecimal é ')
+    print(f'O número {num} convertido em hexadecimal é {hex(num)[2:]}')
+else:
+    print('Opção inválida. Tente novamente!')
