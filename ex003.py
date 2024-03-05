720d20d0c6b97478a6065ea63a537f121a39f0e7
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-03-05 09:59:24 -0300

    exercício 36 mundo 2 de python cursoemvideo.com

--- ex003.py
+++ ex003.py
@@ -1,6 +1,7 @@
 # Crie um programa que leia dois números e mostre a soma entre eles.
 
+cor = dict(fim='\033[m', verm='\033[1;31m', verd='\033[1;32m', amar='\033[1;33m', az='\033[1;34m')
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 s = (n1 + n2)
-print('A soma entre {} e {} vale {}!'.format(n1, n2, s))
+print(f'A soma entre {cor['amar']}{n1}{cor['fim']} e {cor['amar']}{n2}{cor['fim']} vale {cor['az']}{s}{cor['fim']}!')
