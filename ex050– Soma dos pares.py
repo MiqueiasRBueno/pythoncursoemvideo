a514376dcb8485f4f1ca7e1358511aeb1df7107c
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-03-20 11:21:44 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex050– Soma dos pares.py
+++ ex050– Soma dos pares.py
@@ -4,7 +4,7 @@
 soma = 0
 cont = 0
 for c in range(1, 7):
-    num = int(input('Digite um valor: '))
+    num = int(input(f'Digite o {c}°  valor: '))
     if num % 2 == 0:
         soma += num
         cont += 1
