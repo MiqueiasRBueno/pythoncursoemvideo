9ed779544ec4c3dbd2126ddd53917a30ed0e7c6e
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-03-22 08:25:01 -0300

    exercícios de python mundo 1 cursoemvideo.com

--- ex026-Primeira e última ocorrência de uma string.py
+++ ex026-Primeira e última ocorrência de uma string.py
@@ -6,4 +6,4 @@ letra = str(input('Digite a letra que deseja analisar: ')).strip().upper()
 print('A letra \033[1;34m{}\033[m aparece \033[1;34m{}\033[m veze(s) na frase'.format(letra, frase.count(letra)))
 print('A primeira aparição da letra \033[1;32m{}\033[m é na posição '
       '\033[1;32m{}\033[m'.format(letra, frase.find(letra)+1))
-print('A sua última aparição é a \033[1;32m{}\033[m'.format(frase.rfind(letra)-1))
+print('A sua última aparição é a \033[1;32m{}\033[m'.format(frase.rfind(letra)+1))
