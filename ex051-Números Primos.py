0d3694cb52a7c9c229e68cb3421334efab6cd87d
MiqueiasRBueno <miqueisaricardobueno@gmail.com>
2024-03-24 20:29:42 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex051-Números Primos.py
+++ ex051-Números Primos.py
@@ -1,12 +1,14 @@
+from time import sleep
 num = int(input('Digite um número inteiro: '))
 qtd = 0
 num_zero = 0
+print('PROCESSANDO...')
+sleep(1)
 for c in range(1, num + 1):
     primo = num % c
     if primo == 0:
         qtd += 1
-        num_zero = qtd == 2
-if num_zero:
+if qtd == 2:
     print(f'O número {num} é primo!')
 else:
     print(f'O número {num} não é um número primo!')
