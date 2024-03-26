c0345b24444245d651041bfbe2a19fc09aa6bb78
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-03-26 11:39:20 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex054- Grupo da maioridade.py
+++ ex054- Grupo da maioridade.py
@@ -8,7 +8,7 @@ velhos = 0
 for c in range(1, 8):
     num = int(input(f'Em que ano a {c}ª pessoa nasceu? '))
     idade = ano - num
-    jovens += idade <= 18
-    velhos += idade > 18
+    jovens += idade < 21
+    velhos += idade >= 21
 print(f'Ao todo tivemos \033[1;31m{velhos}\033[m pessoas maiores de idade'
       f'\nE também tivemos \033[1;33m{jovens}\033[m pessoas menores de idade. ')
