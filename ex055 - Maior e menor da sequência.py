7913aad171519d3d7ae4c236e8fd521cb8d6e450
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-03-27 07:58:23 -0300

    exercícios de python mundo 2 cursoemvideo.com

---
+++ ex055 - Maior e menor da sequência.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+for pessoas in range(1, 6):
+    peso = float(input(f'O peso da {pessoas}ª pessoa é: '))
+    
