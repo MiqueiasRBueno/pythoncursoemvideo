fff8b20d3afea540650245ab1bec227ae8b2f58a
MiqueiasRBueno <miqueisaricardobueno@gmail.com>
2024-03-29 00:00:02 -0300

    exercícios do mundo 1 python cursoemvideo.com

--- ex034– Aumentos múltiplos.py
+++ ex034– Aumentos múltiplos.py
@@ -1,3 +1,13 @@
 # Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
 # Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
 
+sal_func = float(input('Qual o salário do funcionário: '))
+sal_min = float(input('Qual valor do salário mínimo? '))
+if sal_func <= sal_min:
+    nov_sal = sal_func + (sal_func * 0.15)
+    print(f'O salário atual do funcionário é de R$\033[1;32m{sal_func:.2f}\033[m\n'
+          f'Com o acréscimo de \033[33m15%\033[m passará a ser de R$\033[1;32m{nov_sal:.2f}\033[m')
+else:
+    nov_sal = sal_func + (sal_func * 0.1)
+    print(f'O salário atual do funcionário é de R$\033[1;34m{sal_func:.2f}\033[m\n'
+          f'Com o acréscimo de \033[1;33m10%\033[m passará a ser de R$\033[1;34m{nov_sal:.2f}\033[m')
--- ex035– Analisando Triângulo v1.0.py
+++ ex035– Analisando Triângulo v1.0.py
@@ -1,2 +1,15 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem
 # ou não formar um triângulo.
+
+print('\033[1;32m=' * 7, '\033[mANALISANDO TRIÂNGULOS', '\033[1;32m=' * 7, '\033[m')
+seg_1 = int(input('Digite o valor do 1º seguimento: '))
+seg_2 = int(input('Digite o valor do 2º segmento: '))
+seg_3 = int(input('Digite o valor do 3º segmento: '))
+if seg_1 + seg_2 > seg_3 and seg_1 + seg_3 > seg_2 and seg_2 + seg_3 > seg_1:
+    print(f'Os segmentos digitados FORMAM um triângulo!')
+    if seg_1 != seg_2 != seg_3 != seg_1:
+        print(f'Temos um triângulo \033[1;31mESCALENO\033[m!')
+    if seg_1 == seg_2 == seg_3 == seg_1:
+        print(f'Temos um triângulo \033[1;33mEQUILÁTERO\033[m!')
+    if seg_1 != seg_2 == seg_3 != seg_1:
+        print(f'Temos um triângulo \033[1;35mISÓSCELES\033[m!')
