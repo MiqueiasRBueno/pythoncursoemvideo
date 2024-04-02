a455c71f82013101ab09780c284ac86c033e899a
MiqueiasRBueno <miqueisaricardobueno@gmail.com>
2024-04-01 23:31:24 -0300

    exercícios do mundo 2 python cursoemvideo.com

--- ex057.py
+++ ex057.py
@@ -1,9 +1,11 @@
-gen = ''
+gen = str(input('Informe seu sexo [ F/ M]: ')).upper()
 gen2 = ''
-sexo = str(input('Informe seu sexo [ F/ M]: ')).upper()
 while gen != "F" and gen != "M":
-    gen2 = str(input('Dados incorretos! Por favor, informe seu sexo [ F / M ]: ')).upper()
-if gen2 != 'F' and gen2 != 'M':
-    print('Sexo {} registrado com sucesso!'.format(gen))
-else:
-    print('Sexo {} registrado com sucesso!'.format(gen2))
+
+    if gen != 'F' and gen != 'M':
+        gen2 = str(input('Dados incorretos! Por favor, informe seu sexo [ F / M ]: ')).upper()
+        if gen2 == 'F' and gen2 == 'M':
+            print('Sexo {} registrado com sucesso!'.format(gen2))
+        else:
+            print('Sexo {} registrado com sucesso!'.format(gen2))
+print('Sexo {} registrado com sucesso!'.format(gen))
