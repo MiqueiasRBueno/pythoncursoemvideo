1b742347a613ca83bb13b61383e285714063266c
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-02 08:59:12 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex057 – Validação de Dados.py
+++ ex057 – Validação de Dados.py
@@ -1,7 +1,11 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
 # Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
-sexo = str(input('Digite seu sexo [ F/M ]: ')).upper()
-while sexo != 'F' and sexo != 'M':
-    sexo = str(input('Dados inválidos. Por favor, digite seu sexo [F/M]: ')).upper()
-print(f'Sexo {sexo} registrado com sucesso!')
+sexo = str(input('Digite seu sexo [ F/M ]: ')).upper()  # Contém a condição a ser comparada
+while sexo != 'F' and sexo != 'M':  # Compara se a condição é verdadeira ou falsa
+    sexo = str(input('Dados inválidos. Por favor, digite seu sexo [F/M]: ')).upper()  # se condição for falsa essa
+    # condição passa a ser verificada se verdadeira ou falsa.
+if sexo == 'F':
+    print(f'Sexo \033[1;35mFEMININO\033[m registrado com sucesso!')
+elif sexo == 'M':
+    print('Sexo \033[1;32mMASCULINO\033[m validado com sucesso!')
