7855960ac0a71df698dcb18df33d0c5386401572
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-04 17:32:03 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex061 - Progressão Aritmética v2.0.py
+++ ex061 - Progressão Aritmética v2.0.py
@@ -1,11 +1,13 @@
 # Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
 # usando a estrutura while.
-
+from time import sleep
 print('=-=' * 5, 'Gerador de P.A.', '=-=' * 5)
 termo = int(input('Digite o primeiro termo da P.A.: '))
 rz = int(input('Qual a razão da P.A.: '))
 n = 0
 while n < 10:
+    sleep(0.5)
     n += 1
     pa = termo + (n - 1) * rz
-    print(pa, end=' ')
+    print(pa, '\033[1;31m¬\033[m', end=' ')
+print('\033[1;32mAcabou!\033[m')
