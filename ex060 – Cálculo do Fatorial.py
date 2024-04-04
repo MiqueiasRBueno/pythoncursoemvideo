a0921bb605b5935b68a73f350e83c3953401f801
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-04 14:27:18 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex060 – Cálculo do Fatorial.py
+++ ex060 – Cálculo do Fatorial.py
@@ -1,10 +1,15 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
 # 5! = 5 x 4 x 3 x 2 x 1 = 120
-
+from time import sleep
 num = int(input('Digite um número para calcular seu fatorial: '))
 ft = num
 mult = 1
-print(f'{num}! = {num}', end=' ')
+sleep(0.5)
+print(f'Calculando', end='')
+for c in range(0, 6):
+    sleep(1)
+    print('.', end='')
+print(f' {num}! = {num}', end=' ')
 while ft != 1:
     ft -= 1
     mult *= ft + 1
