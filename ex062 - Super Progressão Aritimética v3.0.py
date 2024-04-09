4a7cd554cfe0095cada95dcfe822d549a9285846
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-09 10:12:11 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex062 - Super Progressão Aritimética v3.0.py
+++ ex062 - Super Progressão Aritimética v3.0.py
@@ -5,14 +5,12 @@ termo = int(input('Insira o primeiro termo de sua P.A.: '))
 razao = int(input('Insira a razão para sua P.A.: '))
 total = 0  # Será somado ao número de termo inicial com os termos adicionais
 mais = 10  # Número de termos da p.a inicial
-soma = 0   # somará a repetição + 1
 n = 0  # localização do termo da pa
 while mais != 0:
     total = total + mais
     while n != total:
-        soma += 1
         n += 1
         pa = termo + (n - 1) * razao
         print(pa, end='\033[1;31m ¬ \033[m')
     mais = int(input('\033[1;32mPAUSA\033[m\nQuantos termos você quer mostrar a mais? '))
-print('\033[1;31mP.A.\033[m finalizada com \033[1;33m{}\033[m termos mostrados.'.format(soma))
+print('\033[1;31mP.A.\033[m finalizada com \033[1;33m{}\033[m termos mostrados.'.format(total))
---
+++ ex063 – Sequência de Fibonacci v1.0.py
@@ -0,0 +1,19 @@
+# Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência
+# de Fibonacci. Exemplo:
+# 0 – 1 – 1 – 2 – 3 – 5 – 8
+
+print('\033[1;31m-=-\033[m' * 30)
+print(' ' * 25, 'SEUQUENCIA DE FIBONACCI', )
+print('\033[1;31m-=-\033[m' * 30)
+n = int(input("Quantos termos deseja mostrar: "))
+t1 = 0  # valor do primeiro termo
+t2 = 1  # valor do segundo termo
+count = 3  # contará a partir do 3º termo
+print(t1, '\033[1;31m¬\033[m', t2, end=' \033[1;31m¬\033[m ')
+while count <= n:  # enquanto count menor ou igual a 'n', a repetição sera executada
+    count += 1
+    t3 = t1 + t2
+    t1 = t2
+    t2 = t3
+    print(t3, end='\033[1;31m ¬\033[m ')
+print('\033[1;32mFIM\033[m')
