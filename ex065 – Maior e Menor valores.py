7c31c03da987fa3300e61a3e68945260529ca5c1
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-17 17:29:15 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex065 – Maior e Menor valores.py
+++ ex065 – Maior e Menor valores.py
@@ -2,3 +2,13 @@
 # os valores e qual foi o maior e o menor valores lidos.
 # O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
+sn = ''
+num = 0
+soma = 0
+cont = 0
+while sn != 'N':
+    num = int(input('Digite um número: '))
+    sn = str(input('Quer continuar? [S/N] ')).upper().strip()
+    soma += num
+    cont += 1
+print(soma / cont)
