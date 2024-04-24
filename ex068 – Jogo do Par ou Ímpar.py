de0f8e84099a526e47319aca3888b2dc92f00c5a
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-24 15:57:49 -0300

    exercícios de python mundo 2 cursoemvideo.com

--- ex068 – Jogo do Par ou Ímpar.py
+++ ex068 – Jogo do Par ou Ímpar.py
@@ -2,12 +2,12 @@
 # mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 
 from random import randint
-pc = randint(0, 100)
 print('VAMOS JOGAR PAR OU IMPAR?')
-print(pc)
 soma = 0
 while True:
+    print('-' * 40)
     num = int(input('Digite um valor: '))
+    pc = randint(0, 100)
     par_impar = str(input('Par ou Impar? [P/I] ')).upper().strip()
     par = (num + pc) % 2
     if par_impar == 'P' and par != 0:
@@ -21,5 +21,7 @@ while True:
         print(f'Você jogou {num} e o computador {pc}. Total de {num + pc} DEU IMPAR!')
         print('Você venceu!\nVamos jogar novamente...')
     soma += 1
+print('-' * 40)
 print(f'Você ganhou {soma} veze(s)')
+print('=' * 40)
 print('GAME OVER, você perdeu!')
