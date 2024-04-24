0b9ad23ae1e7bd86d47e93bdb9a6d6ba05e7f402
MiqueiasRBueno <miqueisaricardobueno@gmail.com>
2024-04-23 23:16:49 -0300

    exercícios do mundo 2 python cursoemvideo.com

--- ex067 – Tabuada v3.0.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O
-# programa será interrompido quando o número solicitado for negativo.
-
-mult = 0
-
-while True:
-    num = int(input('Quer ver a tabuada de qual valor? '))
-    mult += 1
-    print(f'{num} X {mult} = {mult * num}')
