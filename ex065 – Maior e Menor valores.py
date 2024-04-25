7815c43b57aff2408f43ef48ac40d3e37c31bbc2
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-24 21:35:09 -0300

    exercícios de python mundo 2 cursoemvideo.com

---
+++ ex065 – Maior e Menor valores.py
@@ -0,0 +1,19 @@
+# Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os
+# valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou
+# não continuar a digitar valores.
+
+soma = cont = maior = menor = conti = 0
+while conti != 'N':
+    num = int(input('Digite um número: '))
+    conti = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    soma += num
+    cont += 1
+    if cont == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+print(f'Você digitou {cont} e a média entre eles é de {soma / cont}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
