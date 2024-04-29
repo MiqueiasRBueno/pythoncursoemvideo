ac09b0a7d5b0e050764ac6de63d665712c7bbf25
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-29 15:21:09 -0300

    exercícios de python mundo 2 cursoemvide.com

--- ex 071 - Simulador de Caixa Eletrônico.py
+++ ex 071 - Simulador de Caixa Eletrônico.py
@@ -1,6 +1,27 @@
-num = float(input('Qual valor quer sacar? '))
-cinq = num // 50
-vinte = num % 50 // 20
-dez = num % 50 % 20 // 10
-cinco = num % 50 % 10 // 5
-print(cinq, vinte, dez, cinco)
+# Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o
+# valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
+# considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+print('=' * 40, '\n{:^40}'.format('BANCO PERDIDIMMM'))
+print('=' * 40)
+while True:
+    num = int(input('Qual valor quer sacar? '))
+    cinq = num // 50
+    vinte = num % 50 // 20
+    dez = num % 50 % 20 // 10
+    cinco = num % 50 % 10 // 5
+    um = num % 50 % 5 // 1
+    if cinq > 0:
+        print(f'Foram sacadas {cinq:.0f} notas de R$ 50,00')
+    if vinte > 0:
+        print(f'Foran sacadas {vinte:.0f} notas de R$ 20,00')
+    if dez:
+        print(f'Foram sacadas {dez:.0f} notas de R$ 10,00')
+    if cinco:
+        print(f'Foram sacadas {cinco:.0f} notas de R$ 5,00')
+    if um > 0:
+        print(f'Foram sacadas {um:.0f} notas de R$ 1,00')
+    if num == num:
+        break
+print('=' * 40)
+print('Volte sempre ao BANCO PERDIDIMMM! Tenha um bom dia!')
---
+++ ex071.1 - Simulador de Caixa Eletrônico.py
@@ -0,0 +1,31 @@
+# Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o
+# valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
+# considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+print('=' * 40)
+print('{:^48}'.format('\033[31mBANCO PERDIDIMMM\033[m'))
+print('=' * 40)
+valor = int(input('Digite o valor que deseja sacar: R$ '))
+total = valor
+cedulas = 50
+totalced = 0
+while True:
+    if total >= cedulas:
+        total -= cedulas
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Foram sacadas {totalced} cédula(s) de R$ {cedulas:.2f}')
+        if cedulas == 50:
+            cedulas = 20
+        elif cedulas == 20:
+            cedulas = 10
+        elif cedulas == 10:
+            cedulas = 5
+        elif cedulas == 5:
+            cedulas = 2
+        elif cedulas == 2:
+            cedulas = 1
+        totalced = 0
+        if total == 0:
+            break
