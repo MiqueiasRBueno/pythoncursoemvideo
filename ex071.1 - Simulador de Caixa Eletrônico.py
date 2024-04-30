18f5c5c26038cbdcba8b53ac08b2f0de1420c052
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-04-30 10:27:19 -0300

    exercícios de python mundo 2 cursoemvide.com

--- ex071.1 - Simulador de Caixa Eletrônico.py
+++ ex071.1 - Simulador de Caixa Eletrônico.py
@@ -2,32 +2,28 @@
 # valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
 # considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 
-print('=' * 40)
-print('{:^48}'.format('\033[31mBANCO PERDIDIMMM\033[m'))
-print('=' * 40)
-valor = int(input('Digite o valor que deseja sacar: R$ '))
+print('\033[31m=\033[m' * 40)
+print('{:^40}'.format('BANCO PERDIDIMMM'))
+print('\033[31m=\033[m' * 40)
+valor = int(input('Digite o valor do saque: R$'))
 total = valor
-cedulas = 50
+cedula = 50
 totalced = 0
 while True:
-    if total >= cedulas:
-        total -= cedulas
+    if total >= cedula:
+        total -= cedula
         totalced += 1
     else:
         if totalced > 0:
-            print(f'Foram sacadas {totalced} cédula(s) de R$ {cedulas:.2f}')
-        if cedulas == 50:
-            cedulas = 20
-        elif cedulas == 20:
-            cedulas = 10
-        elif cedulas == 10:
-            cedulas = 5
-        elif cedulas == 5:
-            cedulas = 2
-        elif cedulas == 2:
-            cedulas = 1
+            print(f'Total de {totalced} cedula(s) de R${cedula:.2f}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
         totalced = 0
         if total == 0:
             break
-    print('=' * 40)
-    print('Volte sempre ao BANCO PERDIDIMMM! Tenha um bom dia!')
+print('\033[31m=\033[m' * 40)
+print('Obrigado por vir ao BANCO PERDIDIMMM! \nVolte sempre!')
