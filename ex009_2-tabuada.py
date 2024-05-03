1cb8ad165c716f96eaab2ff417c403c005469610
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-05-03 13:59:11 -0300

    exercícios de python mundo 2 cursoemvide.com

--- ex009_2-tabuada.py
+++ ex009_2-tabuada.py
@@ -1,9 +1,12 @@
 # Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
 
-tab = 0
-num = int(input('Digite um número inteiro: '))
 while True:
-    tab += 1
-    print('{: >2}  \033[31m{: ^2}\033[m{: >2}  \033[31m{:^2}\033[m{: >5}'.format(tab, 'X', num, '=', tab * num))
-    if tab == 10:
+    num = int(input('Digite um número inteiro: '))
+    for tab in range(1, 11):
+        print('{: >2}  \033[31m{: ^2}\033[m{: >2}  \033[31m{:^2}\033[m{: >5}'.format(tab, 'X', num, '=', tab * num))
+    parar = ' '
+    while parar not in 'SsNn':
+        parar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+    if parar == 'N':
         break
+print('Tabuada finalizada com sucesso!')
