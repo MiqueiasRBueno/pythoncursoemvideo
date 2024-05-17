1d779ec9b0512e911c6bfe84d288ce685e1edc94
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-05-17 11:59:32 -0300

    exercícios de python mundo 2 cursoemvide.com

---
+++ ex073 - Tuplas com Times de Futebol.py
@@ -0,0 +1,11 @@
+# Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol,
+# na ordem de colocação. Depois mostre:
+# a) Os 5 primeiros times.
+# b) Os últimos 4 colocados.
+# c) Times em ordem alfabética.
+# d) Em que posição está o time da Chapecoense.
+
+times = ('ATLÉTICO PARANAENSE', 'BAHIA', 'FLAMENGO', 'BOTAFOGO', 'SÃO PAULO',
+         'CRUZEIRO', 'ATLÉTICO MINEIRO', 'BRAGANTINO', 'PALMEIRAS', 'INTERNACIONAL',
+         'FORTALEZA', 'GRÊMIO', 'VASCO', 'CRICIÚMA', 'JUVENTUDE',
+         'CORINTHIANS', 'FLUMINENSE', 'VITÓRIA', 'ATLÉTICO GOIÂNO', 'CUÍABA')
