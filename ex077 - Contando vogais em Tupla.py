03d2d93026c1c9440354dcd05040ac9eb2a9898b
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-05-22 14:34:06 -0300

    exercícios de python mundo 2 cursoemvide.com

---
+++ ex077 - Contando vogais em Tupla.py
@@ -0,0 +1,9 @@
+# Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
+# Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+
+palavras = ('IGREJA', 'CARRO', 'ARMAZEM', 'ESTUDO', 'AVIAO', 'PASSARO', 'HIPOPOTAMO')
+for vg in palavras:
+    print(f'\nNa palavra \033[32;1m{vg}\033[m temos as vogais ', end='')
+    for vogais in vg:
+        if vogais in 'AEIOU':
+            print(f'\033[32;1m{vogais}\033[m', end='')
