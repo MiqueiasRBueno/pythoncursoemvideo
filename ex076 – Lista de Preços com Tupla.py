4b0a852ea88da747627bbc55ad326ea17c5c1b42
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-05-22 11:58:50 -0300

    exercícios de python mundo 2 cursoemvide.com

--- ex076 – Lista de Preços com Tupla.py
+++ ex076 – Lista de Preços com Tupla.py
@@ -1,14 +1,17 @@
 # Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
 # No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
-lista = ('lápis............................R$ \033[31m1,75\033[m',
-         'caneta...........................R$ \033[31m3,50\033[m',
-         'caderno..........................R$\033[31m12,00\033[m',
-         'borracha.........................R$ \033[31m2,50\033[m',
-         'cola.............................R$ \033[31m6,75\033[m')
+lista = ('lápis', 1.75,
+         'caneta', 3.50,
+         'caderno', 12.00,
+         'borracha', 2.50,
+         'cola', 6.75)
 print('-' * 40)
-print('{:^48}'.format('\033[31mTABELA DE PREÇOS\033[m'))
+print('{:^48}'.format('\033[34mTABELA DE PREÇOS\033[m'))
 print('-' * 40)
-for ob in lista:
-    print(ob)
+for pos in range(0, len(lista)):
+    if pos % 2 == 0:
+        print(f'{lista[pos]:.<27}', end='')
+    else:
+        print(f'R$\033[31m{lista[pos]:>7.2f}\033[m')
 print('-' * 40)
