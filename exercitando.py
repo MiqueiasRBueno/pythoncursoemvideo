b9b358397124c8d96f5f7cbde6e7ff443ddf9980
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-05-22 10:44:58 -0300

    exercícios de python mundo 2 cursoemvide.com

--- exercitando.py
+++ exercitando.py
@@ -1,26 +1,3 @@
-# Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o
-# valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
-# considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
-
-valor = int(input('Digite o valor que deseja sacar: '))
-total = valor
-cedula = 50
-totalced = 0
-while True:
-    if total >= cedula:
-        total -= cedula
-        totalced += 1
-    else:
-        if totalced > 0:
-            print(f'Total de {totalced} cédulas de {cedula}')
-        elif cedula == 50:
-            cedula = 20
-        elif cedula == 20:
-            cedula = 10
-        elif cedula == 10:
-            cedula = 5
-        elif cedula == 5:
-            cedula = 2
-        totalced = 0
-        if total == 0:
-            break
+nomes = ["Ana", "Carlos", "Maria"]
+for i in range(len(nomes)):
+    print("Nome", i+1, ":", nomes[i])
