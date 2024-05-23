3aefc621f6609c9fa061d8a5d22547ed1b99da5a
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-05-23 09:34:02 -0300

    exercícios de python mundo 2 cursoemvide.com

--- ex 075 - Análise de dados em uma Tupla.py
+++ ex 075 - Análise de dados em uma Tupla.py
@@ -7,13 +7,13 @@ num = (int(input('Digite um número: ')), int(input('Digite mais um número: '))
        int(input('Digite outro número: ')), int(input('Digite mais outro número: ')))
 print(f'Você digitou os valores: ', end='')
 for n1 in num:
-    print(n1, end=' ')
-print(f'\nO valor 9 apareceu {num.count(9)} vezes')
+    print(f'\033[31m{n1}\033[m', end=' ')
+print(f'\nO valor \033[33;1m9\033[m apareceu \033[31m{num.count(9)}\033[m vezes')
 if 3 in num:
-    print(f'O primeiro valor 3 apareceu na {num.index(3) + 1}ª posição')
+    print(f'O primeiro valor \033[1;33m3\033[m apareceu na \033[33;1m{num.index(3) + 1}ª\033[m posição')
 else:
-    print('Não temos valore 3 em nossa tupla')
+    print('Não temos valore \033[1;33m3\033[m em nossa tupla')
 print(f'Os valores pares são', end=' ')
 for n in num:
     if n % 2 == 0:
-        print(n, end=', ')
+        print(f'\033[31m{n}\033[m', end=', ')
