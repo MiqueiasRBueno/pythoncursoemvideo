b82cd61ab988b1d6dec112404dbbb5ca3ac743c3
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-06-05 12:20:32 -0300

    exercícios de python mundo 2 cursoemvide.com

--- ex080 – Lista ordenada sem repetições.py
+++ ex080 – Lista ordenada sem repetições.py
@@ -4,15 +4,16 @@
 lista = []
 for c in range(0, 5):
     num = int(input(f'Digite o {c + 1}º valor: '))
-    if c == 0 or num > lista[-1]:
-        lista.append(num)
-        print('Este valor vai para o final da lista...')
-    else:
-        pos = 0
-        while pos <= len(lista):
-            if num < lista[pos]:
-                lista.insert(pos, num)
-                print(f'Este valor foi adicionado na posição {pos} da lista...')
-                break
-            pos += 1
+    if num not in lista:
+        if c == 0 or num > lista[-1]:
+            lista.append(num)
+            print('Este valor vai para o final da lista...')
+        else:
+            pos = 0
+            while pos <= len(lista):
+                if num < lista[pos]:
+                    lista.insert(pos, num)
+                    print(f'Este valor foi adicionado na posição {pos} da lista...')
+                    break
+                pos += 1
 print(f"Os valores digitados em ordem foram: {lista}")
