338386fc83533c2ee04385e13da2debda4308ff1
MiqueiasRBueno <miqueisaricardobueno@gmail.com>
2024-06-08 22:52:25 -0300

    exercicios curso em vide mundo 1

--- ex031 -Custo da Viagem.py
+++ ex031 -Custo da Viagem.py
@@ -1,5 +1,5 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem, cobrando R$0,50
-# por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
+# por km para viagens de até 200Km e R$0,45 parta viagens mais longas.
 
 distancia = float(input('Qual a distância à percorrer um sua viagem: '))
 if distancia <= 200:
