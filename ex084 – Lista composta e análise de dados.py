480f55f2791c2968bf3bfbd5d4e0c1b420215d7b
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-07-24 14:58:18 -0300

    exercícios de python mundo 3 cursoemvideo.com

--- ex084 – Lista composta e análise de dados.py
+++ ex084 – Lista composta e análise de dados.py
@@ -12,17 +12,10 @@ while True:
     pessoas_cadastradas.append(str(input('Cadastre o nome da pessoa: ')))
     pessoas_cadastradas.append(int(input('Cadastre o peso da pessoa: ')))
     total_pessoas += 1
-    if pessoas_cadastradas == 1:
-        maior = menor = pessoas_cadastradas
-    else:
-        if pessoas_cadastradas > maior:
-            pessoas_mais.append(pessoas_cadastradas[:])
-        if pessoas_cadastradas < menor:
-            pessoas_menos.append(pessoas_cadastradas[:])
     parar = ' '
     while parar not in 'SN':
         parar = str(input('Quer continuar? [S/N]> ')).upper().strip()[0]
     if parar == 'N':
         break
 print(f'O total de pessoas cadastradas foi de {total_pessoas} pessoas')
-print(f'O maior peso é de  {pessoas_mais}kg. O peso de {pessoas_mais}')
+print(pessoas_cadastradas)
