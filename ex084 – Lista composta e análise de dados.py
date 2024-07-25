ae7e13f509ea21079253dfe2ecf15b99283a4fa0
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-07-25 16:23:37 -0300

    exercícios de python mundo 3 cursoemvideo.com

--- ex084 – Lista composta e análise de dados.py
+++ ex084 – Lista composta e análise de dados.py
@@ -4,18 +4,31 @@
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
 
-pessoas_cadastradas = list()
-pessoas_mais = list()
-pessoas_menos = list()
-total_pessoas = maior = menor = 0
+cadastro_temp = []
+cadastro_princ = []
+peso_maior = peso_menor = 0
 while True:
-    pessoas_cadastradas.append(str(input('Cadastre o nome da pessoa: ')))
-    pessoas_cadastradas.append(int(input('Cadastre o peso da pessoa: ')))
-    total_pessoas += 1
-    parar = ' '
-    while parar not in 'SN':
-        parar = str(input('Quer continuar? [S/N]> ')).upper().strip()[0]
-    if parar == 'N':
+    cadastro_temp.append(str(input('Cadastre um nome: ')))
+    cadastro_temp.append(float(input('Cadastre o peso:')))
+    if len(cadastro_princ) == 0:
+        peso_maior = peso_menor = cadastro_temp[1]
+    else:
+        if cadastro_temp[1] > peso_maior:
+            peso_maior = cadastro_temp[1]
+        if cadastro_temp[1] < peso_menor:
+            peso_menor = cadastro_temp[1]
+    cadastro_princ.append(cadastro_temp[:])
+    cadastro_temp.clear()
+    parar = (str(input('Quer continuar? [S/N] ')))
+    if parar in 'Nn':
         break
-print(f'O total de pessoas cadastradas foi de {total_pessoas} pessoas')
-print(pessoas_cadastradas)
+print(f'Foram cadastradas o total de {len(cadastro_princ)} pessoas.')
+print(f'O maior peso cadastrado foi {peso_maior}kg. O peso de ', end=' ')
+for p in cadastro_princ:
+    if p[1] == peso_maior:
+        print([p[0]], end=' ')
+print()
+print(f'O menor peso cadastrado foi {peso_menor}kg. O peso de ', end=' ')
+for p in cadastro_princ:
+    if p[1] == peso_menor:
+        print([p[0]], end=' ')
