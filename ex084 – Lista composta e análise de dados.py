8d0d8e3c8db3d3a1bcd1cfb82dab1154fe21d50e
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-07-29 15:26:15 -0300

    exercícios de python mundo 3 cursoemvideo.com

--- ex084 – Lista composta e análise de dados.py
+++ ex084 – Lista composta e análise de dados.py
@@ -4,33 +4,36 @@
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
 
-temp = []
-princ = []
+cadastro_temp = list()
+cadastro_princ = list()
 maior = menor = 0
 while True:
-    temp.append(str(input('Cadastre um nome: ')).title().strip())
-    temp.append(float(input('Cadastre o peso: ')))
-    if len(princ) == 0:
-        maior = menor = temp[1]
+    cadastro_temp.append(str(input('Nome: ')).title().strip())
+    cadastro_temp.append(float(input('Peso: ')))
+    if len(cadastro_princ) == 0:
+        maior = menor = cadastro_temp[1]
     else:
-        if (temp[1]) > maior:
-            maior = temp[1]
-        if (temp[1]) < menor:
-            menor = temp[1]
-    princ.append(temp[:])
-    temp.clear()
+        if cadastro_temp[1] > maior:
+            maior = cadastro_temp[1]
+        if cadastro_temp[1] < menor:
+            menor = cadastro_temp[1]
+    cadastro_princ.append(cadastro_temp[:])
+    cadastro_temp.clear()
     parar = ' '
     while parar not in 'SN':
-        parar = (str(input('Deseja continuar? [S/N] '))).strip().upper()[0]
+        parar = str(input('Quer continuar? [S/N]: ')).title().strip()[0]
     if parar == 'N':
         break
-print(f'Foram cadastradas {len(princ)} pessoas.')
-print(f'O maior peso cadastrado foi {maior:.2f}\033[31;1mkg\033[m. Peso de : ', end=' ')
-for p in princ:
+print('=*=' * 30)
+print(f'O número de pessoas cadastradas são {len(cadastro_princ)} pessoas.')
+print(f'O maior peso cadastrado é de {maior}kg. Peso de ', end=' ')
+for p in cadastro_princ:
     if p[1] == maior:
-        print(f'{p[0]}', end=' ')
+        print(f'[{p[0]}]', end=' ')
 print()
-print(f'O menor peso foi de {menor:.2f}\033[32;1mkg\033[m.', end=' ')
-for p in princ:
+print(f'O menor peso cadastrado é de {menor}kg. Peso de ', end=' ')
+for p in cadastro_princ:
     if p[1] == menor:
-        print(f'{p[0]}', end=' ')
+        print(f'[{p[0]}]', end=' ')
+print()
+print('=*=' * 30)
