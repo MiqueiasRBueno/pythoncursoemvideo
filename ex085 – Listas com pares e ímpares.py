fdb15587355f103ee1e0b514d1ee7aff62a3d14a
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2024-08-16 14:32:51 -0300

    exercícios de python cursoemvideo.com

--- ex085 – Listas com pares e ímpares.py
+++ ex085 – Listas com pares e ímpares.py
@@ -2,21 +2,14 @@
 # lista única que mantenha separados os valores pares e ímpares.
 # No final, mostre os valores pares e ímpares em ordem crescente.
 
-num_temp = []
-num_cadastrados = []
-num_pares = []
-num_impares = []
-pos = 0
-for num in range(0, 7):
-    pos += 1
-    num_temp.append(int(input(f'Digite o {pos}º número: ')))
-    num_cadastrados.append(num_temp[:])
-    for valor in num_temp:
-        if valor % 2 == 0:
-            num_pares.append(valor)
-        else:
-            num_impares.append(valor)
-    num_temp.clear()
-print(f'Lista de números cadastrados: {num_cadastrados}')
-print(f'Os números pares cadastrados foram: {sorted(num_pares)}')
-print(f'Os números ímpares cadastrados foram: {sorted(num_impares)}')
+num_cadastrados = [[], []]
+valores = 0
+for c in range(1, 8):
+    valores = int(input(f'Digite o {c}° valor: '))
+    if valores % 2 == 0:
+        num_cadastrados[0].append(valores)
+    else:
+        num_cadastrados[1].append(valores)
+print('\033[31m-=\033[m' * 30)
+print(f'Valores pares cadastrados: {sorted(num_cadastrados[0])}')
+print(f'Valores impares cadastrados: {sorted(num_cadastrados[1])}')
