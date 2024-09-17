7a40188e256c095aeebfadec8e20c679ceb61507
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2024-09-17 11:08:00 -0300

    python mundo 3 cursoemvideo.com

--- exercitando.py
+++ exercitando.py
@@ -1,20 +1,31 @@
-from random import randint
-lista = []
-jogos = []
-qtd = int(input('Quantidade de jogos: '))
-total = 1
-while total <= qtd:
-    cont = 0
-    while True:
-        num = randint(1, 60)
-        if num not in lista:
-            lista.append(num)
-            cont += 1
-        if cont >= 6:
-            break
-    lista.sort()
-    jogos.append(lista[:])
-    lista.clear()
-    total += 1
-for i, l in enumerate(jogos):
-    print(f'Jogos número {i + 1}: {l}')
+from time import sleep
+
+ficha = []
+while True:
+    nome = str(input('Nome do aluno: ')).title().strip()
+    nota_1 = float(input('Nota 1: '))
+    nota_2 = float(input('Nota 2: '))
+    media = (nota_1 + nota_2) / 2
+    ficha.append([nome, [nota_1, nota_2], media])
+    parar = ' '
+    while parar not in 'SN':
+        parar = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
+    if parar == 'N':
+        break
+print('-=' * 30)
+print(f'{'N°':<4}{'NOME DO ALUNO':<10}{'MÉDIA':>8}')
+print('-' * 25)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{media:>11.1f}')
+    print('-' * 25)
+while True:
+    aluno = int(input('Qual aluno deseja mostrar as notas? (999 interrompe): '))
+    if aluno == 999:
+        break
+    if aluno <= len(ficha) -1:
+        print(f'{'ALUNO':<10}{'NOTAS':>13}')
+        print('-' * 25)
+        print(f'{ficha[aluno][0]:<15}{ficha[aluno][1]}')
+        print('-' * 25)
+sleep(0.5)
+print('Finalizando...')
