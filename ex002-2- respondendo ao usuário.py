f8300af616641815a77c73c66ec0424d69a77377
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2024-10-02 14:43:50 -0300

    Update ex002-2- respondendo ao usuário.py

--- ex002-2- respondendo ao usuário.py
+++ ex002-2- respondendo ao usuário.py
@@ -1,10 +1,4 @@
 # Crie um script que leia o nome de uma pessoa e mostre uma mensagem de Boas-vindas segundo o valor digitado:
 
-while True:
-    nome = str(input('Digite seu nome: ')).strip().title()
-    print(f'\033[32mOlá {nome}, é um prazer te conhecer!\033[m')
-    segue = ' '
-    while segue not in 'SN':
-        segue = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
-    if segue == 'N':
-        break
+nome = str(input('Digite seu nome: ')).strip().title()
+print(f'\033[32mOlá {nome}, é um prazer te conhecer!\033[m')
