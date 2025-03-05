6807f492ed1b378144badfa2a7598146818109d9
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-05 11:37:07 -0300

    correção

--- ex018-Seno, Cosseno e Tangente.py
+++ ex018-Seno, Cosseno e Tangente.py
@@ -1,11 +1,9 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 
-from math import radians, sin, cos
+from math import radians, sin, cos, tan
 grau = float(input('Digite o valor de um ângulo desejado: '))
-rd = radians(grau)
-sen = sin(rd)
-cos = cos(rd)
 print(f'O ângulo de {grau} tem:'
-      f'\nSeu radiano {rd:.2f}'
-      f'\nSeu seno {sen:.2f}'
-      f'\nE seu cosseno {cos:.2f}')
+      f'\nSeu radiano {radians(grau):.2f}'
+      f'\nSeu seno {sin(radians(grau)):.2f}'
+      f'\nE seu cosseno {cos(radians(grau)):.2f}'
+      f'\nE sua tangente é {tan(radians(grau)):.2f}')
