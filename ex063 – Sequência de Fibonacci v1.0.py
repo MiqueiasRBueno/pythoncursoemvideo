af955d45940893f58f307d9ef8cd08d58c468611
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-10 15:28:06 -0300

    correção

--- ex063 – Sequência de Fibonacci v1.0.py
+++ ex063 – Sequência de Fibonacci v1.0.py
@@ -3,14 +3,14 @@
 # 0 – 1 – 1 – 2 – 3 – 5 – 8
 
 print('\033[1;31m-=-\033[m' * 30)
-print(' ' * 25, 'SEUQUENCIA DE FIBONACCI', )
+print(' ' * 25, 'SEQUÊNCIA DE FIBONACCI', )
 print('\033[1;31m-=-\033[m' * 30)
 n = int(input("Quantos termos deseja mostrar: "))
 t1 = 0  # valor do primeiro termo
 t2 = 1  # valor do segundo termo
 count = 3  # contará a partir do 3º termo
 print(t1, '\033[1;31m¬\033[m', t2, end=' \033[1;31m¬\033[m ')
-while count <= n:  # enquanto count menor ou igual a 'n', a repetição sera executada
+while count <= n:  # enquanto count menor ou igual a 'n', a repetição será executada
     count += 1
     t3 = t1 + t2
     t1 = t2
