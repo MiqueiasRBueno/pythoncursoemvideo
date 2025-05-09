7a5ad6a86fa7aa49d889404c8ff5aaf772f25e9c
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-05-08 23:27:03 -0300

    update - correção da descrição do código

--- ex090 – Dicionário em Python.py
+++ ex090 – Dicionário em Python.py
@@ -1,2 +1,16 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 # No final, mostre o conteúdo da estrutura na tela.
+
+boletim = dict()
+boletim['nome'] = str(input('Nome: ')).title().strip()
+boletim['média'] = float(input(f'Média de {boletim["nome"]}: '))
+if boletim['média'] >= 7:
+    boletim['situação'] = 'Aprovado'
+elif 5 < boletim['média'] < 7:
+    boletim['situação'] = 'Recuperação'
+else:
+    boletim['situação'] = 'Reprovado'
+print(f'\033[1;31m{'=' * 55}\033[m')
+for k, v in boletim.items():
+    print(f'- {f'{k}'.title()} é igual a {v}')
+print(f'\033[1;31m{'=' * 55}\033[m')
