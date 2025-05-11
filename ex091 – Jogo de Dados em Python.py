cbc61fe78e86d715d51c05cbc56e0150f7fc2b45
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-05-11 00:03:34 -0300

    update - correção da descrição do código

--- ex091 – Jogo de Dados em Python.py
+++ ex091 – Jogo de Dados em Python.py
@@ -6,9 +6,11 @@ from operator import itemgetter
 from random import randint
 from time import sleep
 
-print(f'''\033[1;31m{'=' * 55}\033[m
-{'JOGO DE DADOS':^55}
-\033[1;31m{'=' * 55}\033[m''')
+print(f'\033[1;31m{'=' * 55}\033[m')
+sleep(1)
+print(f'{'JOGO DE DADOS':^55}')
+sleep(1)
+print(f'\033[1;31m{'=' * 55}\033[m')
 jogos = {'Jogador 01': randint(1, 6), 'Jogador 02': randint(1, 6),
          'Jogador 03': randint(1, 6), 'Jogador 04': randint(1, 6)}
 ranking = []
@@ -18,9 +20,9 @@ for k, v in jogos.items():
     sleep(1)
     print(f'{f'{k} tirou {v}':_^55}')
 sleep(1)
-print(f'''\033[1;31m{'=' * 55}\033[m
-\033[1;33m{'RANKING DE JOGADORES':^55}\033[m
-''')
+print(f'\033[1;31m{'=' * 55}\033[m')
+sleep(1)
+print(f'\033[1;33m{'RANKING DE JOGADORES':^55}\033[m\n')
 ranking = sorted(jogos.items(), key=itemgetter(1), reverse=True)
 for i, v in enumerate(ranking):
     sleep(1)
