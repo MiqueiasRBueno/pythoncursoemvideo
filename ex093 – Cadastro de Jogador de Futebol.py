d11e97c6ddcce302f4ac4f86b788ef21d841e5f3
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-05-12 22:45:00 -0300

    Create - 093 - Cadastro de Jogador de Futebol

--- ex093 – Cadastro de Jogador de Futebol.py
+++ ex093 – Cadastro de Jogador de Futebol.py
@@ -3,24 +3,25 @@
 # Depois vai ler a quantidade de gols feitos em cada partida.
 # No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 
-print(f'''\033[1;31m{'-=' * 20}\033[m
+print(f'''\033[1;31m{'-=' * 40}\033[m
 {'GERENCIADOR DE JOGADORES':^40}
-\033[1;31m{'-=' * 20}\033[m''')
+\033[1;31m{'-=' * 40}\033[m''')
 gerenciador_jogador = dict()
 qtd_gols = list()
 gerenciador_jogador['Nome'] = str(input('Nome do Jogador e ou jogadora: ')).title().strip()
-partidas = int(input(f'Quantas partidas {gerenciador_jogador['Nome']} jogou? '))
+partidas = int(input(f'Quantas partidas \033[1;32m{gerenciador_jogador['Nome']}\033[m jogou? '))
 for c in range(0, partidas):
-    qtd_gols.append(int(input(f'Quanto gols {gerenciador_jogador['Nome']} fez na {c + 1}ª partida? ')))
+    qtd_gols.append(int(input(f'Quanto gols \033[1;32m{gerenciador_jogador['Nome']}\033[m fez na '
+                              f'\033[1;32m{c + 1}\033[mª partida? ')))
     gerenciador_jogador['Gols'] = qtd_gols
     gerenciador_jogador['Total'] = sum(gerenciador_jogador['Gols'])
-print(f'\033[1;31m{'-=' * 20}\033[m')
+print(f'\033[1;31m{'-=' * 30}\033[m')
 for k, v in gerenciador_jogador.items():
-    print(f'O campo {k} tem o valor \033[1;32m{v}\033[m')
-print(f'''\033[1;31m{'-=' * 20}\033[m
-O jogador e ou jogadora {gerenciador_jogador['Nome']} jogou {partidas} partidas, e:
-\033[1;31m{'-=' * 20}\033[m''')
+    print(f'O campo \033[1;32m{k}\033[m tem o valor \033[1;32m{v}\033[m')
+print(f'''\033[1;31m{'-=' * 30}\033[m
+O jogador e ou jogadora \033[1;32m{gerenciador_jogador['Nome']}\033[m jogou \033[1;32m{partidas}\033[m partidas, e:
+\033[1;31m{'-=' * 30}\033[m''')
 tot = 0
 for g in qtd_gols:
-    print(f'_ marcou \033[1;32m{qtd_gols[tot]}\033[m gol(s) na \033[1;32m{g}\033[mª partida ')
+    print(f'_ marcou \033[1;32m{g}\033[m gol(s) na \033[1;32m{1 + tot}\033[mª partida ')
     tot += 1
