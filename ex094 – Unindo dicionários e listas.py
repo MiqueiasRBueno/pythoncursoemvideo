9a709f7ff5b932f0489c2140b59e724cef745133
miqueias <miqueiasricardobueno@gmail.com>
2025-05-15 08:38:22 -0300

    Create - 094 – Unindo dicionários e listas

--- ex094 – Unindo dicionários e listas.py
+++ ex094 – Unindo dicionários e listas.py
@@ -8,16 +8,13 @@
 
 cad_pessoas_dicio = dict()
 cad_pessoas_lista = list()
-mulheres = list()
-indices = media = soma = 0
+media = soma = 0
 while True:
     cad_pessoas_dicio.clear()
     cad_pessoas_dicio['Nome'] = str(input('Nome: ')).title().strip()
     sexo = ' '
     while sexo not in 'MF':
         cad_pessoas_dicio['Sexo'] = str(input('Sexo [M/F]: ')).upper().strip()[0]
-        if cad_pessoas_dicio['Sexo'] in 'F':
-            mulheres.append(cad_pessoas_dicio['Nome'])
         sexo = cad_pessoas_dicio['Sexo']
         if cad_pessoas_dicio['Sexo'] not in 'MF':
             print('ERRO! Por favor, digite somente M ou F!')
@@ -25,9 +22,9 @@ while True:
         break
     idade = int(input('Idade: '))
     cad_pessoas_dicio['Idade'] = idade
-    media += idade / 2
+    soma += idade
     cad_pessoas_lista.append(cad_pessoas_dicio.copy())
-
+    cad_pessoas_dicio.clear()
     stop = ' '
     while stop not in 'SN':
         stop = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
@@ -40,19 +37,15 @@ if len(cad_pessoas_lista) > 1:
 else:
     print(f'A) Foi cadastrada {len(cad_pessoas_lista)} pessoa')
 print(f'B) A média de idade entre as pessoas cadastradas é de {media:.1f} anos')
-if len(mulheres) > 0:
-    if len(mulheres) > 1:
-        print(f'C) As mulheres cadastradas são: ', end=' ')
-    else:
-        print(f'C) A mulher cadastrada é: ', end=' ')
-    for m in mulheres:
-        print(m, end=' ')
-else:
-    print('C) Não temos mulheres cadastradas!')
+print(f'C) As mulheres cadastradas são: ', end=' ')
+for c in cad_pessoas_lista:
+    if c['Sexo'] in 'F':
+        print(c['Nome'], end=' ')
 print()
+media = soma / len(cad_pessoas_lista)
+print('D) As pessoas cadastradas acima da média são:')
 for p in cad_pessoas_lista:
-    if p['Idade'] >= media:
-        print(' ')
-        for k, v in p.itens():
-            print(f'{k} = {v}:', end=' ')
-        print()
+    if p['Idade'] > media:
+        for k, v in p.items():
+            print(f'{k} = {v} ;', end=' ')
+print()
