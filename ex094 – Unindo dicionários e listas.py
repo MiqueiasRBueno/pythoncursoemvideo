9c3e555c1f5a8033048934e8cd5d0f0daec431c8
miqueias <miqueiasricardobueno@gmail.com>
2025-05-15 09:23:17 -0300

    Create - 094 – Unindo dicionários e listas

--- ex094 – Unindo dicionários e listas.py
+++ ex094 – Unindo dicionários e listas.py
@@ -10,7 +10,6 @@ cad_pessoas_dicio = dict()
 cad_pessoas_lista = list()
 media = soma = 0
 while True:
-    cad_pessoas_dicio.clear()
     cad_pessoas_dicio['Nome'] = str(input('Nome: ')).title().strip()
     sexo = ' '
     while sexo not in 'MF':
@@ -20,9 +19,8 @@ while True:
             print('ERRO! Por favor, digite somente M ou F!')
     if sexo == 'MF':
         break
-    idade = int(input('Idade: '))
-    cad_pessoas_dicio['Idade'] = idade
-    soma += idade
+    cad_pessoas_dicio['Idade'] = int(input('Idade: '))
+    soma += cad_pessoas_dicio['Idade']
     cad_pessoas_lista.append(cad_pessoas_dicio.copy())
     cad_pessoas_dicio.clear()
     stop = ' '
@@ -36,13 +34,13 @@ if len(cad_pessoas_lista) > 1:
     print(f'A) Foram cadastradas {len(cad_pessoas_lista)} pessoas')
 else:
     print(f'A) Foi cadastrada {len(cad_pessoas_lista)} pessoa')
-print(f'B) A média de idade entre as pessoas cadastradas é de {media:.1f} anos')
+media = soma / len(cad_pessoas_lista)
+print(f'B) A média de idade entre as pessoas cadastradas é de {media:5.2f} anos')
 print(f'C) As mulheres cadastradas são: ', end=' ')
 for c in cad_pessoas_lista:
     if c['Sexo'] in 'F':
         print(c['Nome'], end=' ')
 print()
-media = soma / len(cad_pessoas_lista)
 print('D) As pessoas cadastradas acima da média são:')
 for p in cad_pessoas_lista:
     if p['Idade'] >= media:
