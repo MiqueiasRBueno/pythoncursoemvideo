71e7b37e11674eea6da50c137f217ffdfcde53f9
miqueias <miqueiasricardobueno@gmail.com>
2025-06-04 10:58:23 -0300

    Create - 105 – Analisando e gerando Dicionários

---
+++ ex105 – Analisando e gerando Dicionários.py
@@ -0,0 +1,35 @@
+# Faça um programa que tenha uma função notas() que pode receber várias notas de alunos
+# e vai retornar um dicionário com as seguintes informações:
+# – Quantidade de notas
+# – A maior nota
+# – A menor nota
+# – A média da turma
+# – A situação (opcional)
+
+# dicionário
+resp = {}
+
+# Função
+def notas(*num, show=False):
+    total = 0
+    for _ in num:
+        total += 1
+        resp['Total'] = total
+        resp['Maior'] = max(num)
+        resp['Menor'] = min(num)
+        soma = sum(num)
+        media = soma / len(num)
+        resp['Média'] = media
+        if show:
+            if media < 5:
+                resp['Situação'] = 'Ruim!'
+            elif media < 7:
+                resp['Situação'] = 'Boa!'
+            else:
+                resp['Situação'] = 'Ótima!'
+    return resp
+
+
+# Programa principal:
+resp = notas(7, 5, 6, 10, 9, show=True)
+print(resp)
