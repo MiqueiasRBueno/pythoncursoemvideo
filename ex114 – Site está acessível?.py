77b5e01ad25620806518d466aa578d9e0869534c
miqueias <miqueiasricardobueno@gmail.com>
2025-06-30 16:39:41 -0300

    create - 114 – Site está acessível?

--- ex114 – Site está acessível?.py
+++ ex114 – Site está acessível?.py
@@ -1,11 +1,10 @@
 # Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
+import urllib
+import urllib.request
 
-import requests
-
-url = "http://www.pudim.com.br"
 try:
-    resp = requests.get(url)
-    resp.raise_for_status()
-    print(f'O site {url} está acessível!')
-except requests.exceptions.RequestException:
-    print(f'O site {url} não está acessível!')
+    url = urllib.request.urlopen("https://greenvirtual.com.br/")
+except urllib.error.URLError:
+    print('O site Pudim não está acessível!')
+else:
+    print('O site Pudim está acessível!')
